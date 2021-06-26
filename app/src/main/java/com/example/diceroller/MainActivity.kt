4050db0617bd70e997e7b1f3390986d0ce96f7af
Hoshino0127 <66240969+Hoshino0127@users.noreply.github.com>
2021-06-26 20:21:24 +0800

    Extra challenge

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -12,14 +12,30 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val btnRoll : Button = findViewById(R.id.Roll_Button)
+        val btnCount : Button = findViewById(R.id.Count_Button)
+
+        val tvResult :TextView = findViewById(R.id.Result_Textview)
 
         btnRoll.setOnClickListener{
             val randomInt = (1..6).random()
 
-            val tvResult :TextView = findViewById(R.id.Result_Textview)
+
 
             tvResult.text = randomInt.toString()
+
             rollDice()
+        }
+
+        btnCount.setOnClickListener{
+            val currentText : String = tvResult.text.toString()
+            when (currentText) {
+                "Hello World!" -> tvResult.text = "1"
+                "6" -> tvResult.text = "6"
+                "1","2","3","4","5" -> { // Note the block
+                    val addUpInt = currentText.toInt() + 1
+                    tvResult.text = addUpInt.toString()
+                }
+            }
         }
     }
 
